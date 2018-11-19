ee6f5a8a2dbe18b1f214cc452e88d6538d634235
Marianne Hoffmann <hhmaho@gmail.com>
2018-11-19 09:38:18 +0100

    state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Marianne", age: 43 },
+      { name: "Max", age: 29 },
+      { name: "Lisa", age: 38 }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
         <p>world!</p>
-        <Person name="Marianne" age="43" />
-        <Person name="Max" age="29">
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           Hobbies: Swimming
         </Person>
-        <Person name="Lisa" age="38" />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />{" "}
+        <button>Switch name</button>
       </div>
     );
   }
